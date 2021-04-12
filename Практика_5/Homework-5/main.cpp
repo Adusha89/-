0168b1f7a9bc7e298eb8d85f8ebe3185793dfbad
Andrey <belyakov.andrey.n@gmail.com>
2021-04-12 21:29:24 +0300

    Доделал пятое задание

--- Практика_5/Homework-5/main.cpp
+++ Практика_5/Homework-5/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <locale.h>
 #include <math.h>
+void five_task()
+{
+
+}
+template <typename FirstArg, typename... Arg>
+void five_task(FirstArg first_arg, Arg... arg)
+{
+    std::cout << (first_arg^1) <<" ";
+    five_task(arg...);
+}
 void first_task(int*, int);
 void second_task(int*,int);
 void print_arr(int*,int);
@@ -21,11 +31,15 @@ int main()
     std::cout << "Заполненный массив для второго задания:\n";
     print_arr(arr2,sizeof (arr2)/sizeof(int));
     int arr3[]={6,6,1,13,26};
+    std::cout << "Результат третьего задания:\n";
     if(third_task(arr3,sizeof (arr3)/sizeof(int))) std::cout << "True\n";
     else std::cout << "False\n";
     int arr4[]={1,2,3,4,5,6};
     four_task(arr4,sizeof (arr4)/sizeof(int),3);
+    std::cout << "Результат четвертого задания:\n";
     print_arr(arr4,sizeof (arr4)/sizeof(int));
+    std::cout << "Результат пятого задания:\n";
+    five_task(1,1,0,0,1,0);//Пятое задание
     return 0;
 }
 
