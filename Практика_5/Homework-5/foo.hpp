2099635062bc05e7d3cb6d4e423dfca45dd051f8
Andrey <belyakov.andrey.n@gmail.com>
2021-04-13 21:48:26 +0300

    Добавил поле имен

--- Практика_5/Homework-5/foo.hpp
+++ Практика_5/Homework-5/foo.hpp
@@ -1,6 +1,10 @@
+
+#ifndef FOO_HPP
+#define FOO_HPP
 #include <iostream>
-//namespace HW5
-//{
+namespace HW5
+{
+void print_arr(int*,int);
 int sum_el_arr(int*,int);
 void five_task()
 {
@@ -14,10 +18,10 @@ void five_task(FirstArg first_arg, Arg... arg)
 }
 void first_task(int*, int);
 void second_task(int*,int);
-void print_arr(int*,int);
 bool third_task(int*,int);
 void four_task(int*,int,int);
 
-//}
+}
 
+#endif // FOO_HPP
 
--- Практика_5/Homework-5/main.cpp
+++ Практика_5/Homework-5/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <locale.h>
 
-#include "foo.h"
-//using namespace HW5;
 #include <math.h>
 #include <iostream>
+#include "foo.hpp"
+using namespace HW5;
 
 
 int main()
@@ -13,26 +13,26 @@ int main()
     int arr[]={1,0,1,1,1,1,0,1,1};//Массив для первого задания
     std::cout << "Оригинальный массив для первого задания:\n";
     void(*fpa)(int*,int)=print_arr;
-    (*fpa)(arr,sizeof (arr)/sizeof(int));
+    fpa(arr,sizeof (arr)/sizeof(int));
     void(*fft)(int*,int)=first_task;
-    (*fft)(arr,sizeof (arr)/sizeof(int));
+    fft(arr,sizeof (arr)/sizeof(int));
     std::cout << "Инвертированный массив для первого задания:\n";
-    (*fpa)(arr,sizeof (arr)/sizeof(int));//Функция печати массива для проверки
+    fpa(arr,sizeof (arr)/sizeof(int));//Функция печати массива для проверки
     int arr2[8]{};//Массив для второго задания
     fft=second_task;
-    (*fft)(arr2,sizeof (arr2)/sizeof(int));
+    fft(arr2,sizeof (arr2)/sizeof(int));
     std::cout << "Заполненный массив для второго задания:\n";
-    (*fpa)(arr2,sizeof (arr2)/sizeof(int));
+    fpa(arr2,sizeof (arr2)/sizeof(int));
     int arr3[]={6,6,1,13,26};
     std::cout << "Результат третьего задания:\n";
     bool(*ftt)(int*,int)=third_task;
-    if((*ftt)(arr3,sizeof (arr3)/sizeof(int))) std::cout << "True\n";
+    if(ftt(arr3,sizeof (arr3)/sizeof(int))) std::cout << "True\n";
     else std::cout << "False\n";
     int arr4[]={1,2,3,4,5,6};
     void(*frt)(int*,int,int)=four_task;
-    (*frt)(arr4,sizeof (arr4)/sizeof(int),3);
+    frt(arr4,sizeof (arr4)/sizeof(int),3);
     std::cout << "Результат четвертого задания:\n";
-    (*fpa)(arr4,sizeof (arr4)/sizeof(int));
+    fpa(arr4,sizeof (arr4)/sizeof(int));
     std::cout << "Результат пятого задания:\n";
     five_task(1,1,0,0,1,0);//Пятое задание
     return 0;
